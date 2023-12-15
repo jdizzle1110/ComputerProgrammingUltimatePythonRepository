bedddd8aab2affc7a6af822ae6c729bf87678968
Jeulz Dean <24jeulzdean@usd266.com>
2023-12-15 10:25:33 -0600

    gradbook/cities

--- 09WorkingWithFiles/Assignment/cities.py
+++ 09WorkingWithFiles/Assignment/cities.py
@@ -2,3 +2,12 @@ import json
 import os
 
 os.chdir(os.path.dirname(os.path.abspath(__file__)))
+
+f = open("1000-largest-us-cities.json", "r")
+words = f.read().split("\n")
+f.close()
+
+
+
+
+
--- 09WorkingWithFiles/Assignment/gradebook.py
+++ 09WorkingWithFiles/Assignment/gradebook.py
@@ -1,4 +1,7 @@
 import csv
+import json
 import os
 
 os.chdir(os.path.dirname(os.path.abspath(__file__)))
+
+f = open("../data/gradebook_data.csv", "r")
